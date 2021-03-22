909bd15742f91191fd371d43040ce6586367a999
Dan Korostelev <nadako@gmail.com>
2021-03-22 11:06:59 +0100

    re-generate test fixtures

--- tests/generated/min_width.rs
+++ tests/generated/min_width.rs
@@ -33,12 +33,12 @@ fn min_width() {
     assert_eq!(stretch.layout(node).unwrap().size.height, 100f32);
     assert_eq!(stretch.layout(node).unwrap().location.x, 0f32);
     assert_eq!(stretch.layout(node).unwrap().location.y, 0f32);
-    assert_eq!(stretch.layout(node0).unwrap().size.width, 80f32);
+    assert_eq!(stretch.layout(node0).unwrap().size.width, 60f32);
     assert_eq!(stretch.layout(node0).unwrap().size.height, 100f32);
     assert_eq!(stretch.layout(node0).unwrap().location.x, 0f32);
     assert_eq!(stretch.layout(node0).unwrap().location.y, 0f32);
-    assert_eq!(stretch.layout(node1).unwrap().size.width, 20f32);
+    assert_eq!(stretch.layout(node1).unwrap().size.width, 40f32);
     assert_eq!(stretch.layout(node1).unwrap().size.height, 100f32);
-    assert_eq!(stretch.layout(node1).unwrap().location.x, 80f32);
+    assert_eq!(stretch.layout(node1).unwrap().location.x, 60f32);
     assert_eq!(stretch.layout(node1).unwrap().location.y, 0f32);
 }
